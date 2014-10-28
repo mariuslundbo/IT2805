e45bfa30bb988175eb21d557614d9f581c8ccc93
andrkje <andreas.pi@hotmail.com>
2014-10-28 22:15:37 +0100

    La til animasjon på slideshow
    La til animasjon på slideshow, og noen bilder i riktig oppløsning.
    Fikset også litt på CSS til midsection

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,25 @@
-var images = ["https://dl.dropboxusercontent.com/u/65639888/image/1.jpg", 
-	"https://dl.dropboxusercontent.com/u/65639888/image/4.jpg", 
-	"https://dl.dropboxusercontent.com/u/65639888/image/5.jpg"];
-var count = 0;
+// var images = ["https://dl.dropboxusercontent.com/u/65639888/image/1.jpg", 
+	// "https://dl.dropboxusercontent.com/u/65639888/image/4.jpg", 
+	// "https://dl.dropboxusercontent.com/u/65639888/image/5.jpg"];
+// var count = 0;
+// 
+// window.setInterval(update, 10000);
+// 
+// function update() {
+	// if (count == 2)
+		// count = 0;
+	// else
+		// count++;
+	// document.getElementById("slideshow").src = images[count];
+// }
 
-window.setInterval(update, 10000);
 
-function update() {
-	if (count == 2)
-		count = 0;
-	else
-		count++;
-	document.getElementById("slideshow").src = images[count];
-}
+// Slideshow animation JQuery
+$("document").ready(function() {
+	
+	$("#slideshow_images").cycle({ 
+		fx: "scrollLeft", 
+    	speed:    600, 
+    	timeout:  5000 });
+    
+});
