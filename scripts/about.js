8a14bc9ef66ca9980515383847d39db2a22eb978
Marius Lundbø <marius.lundbo@gmail.com>
2014-11-01 17:07:00 +0100

    About

---
+++ scripts/about.js
@@ -0,0 +1,7 @@
+var andreas = "andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, andreas, ";
+var marius = "marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, ";
+var stian = "stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, ";
+
+$("#andreas_text").append(andreas);
+$("#marius_text").append(marius);
+$("#stian_text").append(andreas);
