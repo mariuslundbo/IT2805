00a7c8e31a511aff160db3f8bf5bcff97d538ddf
Marius Lundbø <marius.lundbo@gmail.com>
2014-11-02 13:13:56 +0100

    references

--- scripts/about.js
+++ scripts/about.js
@@ -2,10 +2,6 @@ var andreas = "andreas, andreas, andreas, andreas, andreas, andreas, andreas, an
 var marius = "marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, marius, ";
 var stian = "stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, stian, ";
 
-$("#andreas_text").prepend(andreas);
-$("#marius_text").prepend(marius);
-$("#stian_text").prepend(andreas);
-
 $(document).ready(function() {
 	$("#andreas_text").prepend(andreas);
 	$("#marius_text").prepend(marius);
