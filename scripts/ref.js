c4d75ad56d967154358d4f24477aa36d42113995
Marius Lundbø <marius.lundbo@gmail.com>
2014-11-13 19:11:07 +0100

    fix

--- scripts/ref.js
+++ scripts/ref.js
@@ -1,6 +1,6 @@
 var fw = "jQuery from http://jquery.com/ <br/> ajax from http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js";
 var img = "The images alternative.jpg, logo_dark_grey.png, logo_grey.png, logo.png, search_grey.png, search_white.png, search.png are all made by Stian Dysthe. <br />The profile pictures andreas.jpg, marius.jpg and stian.jpg are all private pictures. ";
-var multi = "Multimedia goes here..";
+var multi = "The multimedia from the Work page is made by Stian Dysthe.";
 
 $(document).ready(function() {
 	$("#frameworks").prepend(fw);
