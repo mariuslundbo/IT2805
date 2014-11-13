32a8dc1d287e9eb37b106ee35fe1c7efd4b233b9
andrkje <andreas.pi@hotmail.com>
2014-11-13 18:14:08 +0100

    PHP validering + erros-side hvis email ikke blir sendt

--- scripts/email.php
+++ scripts/email.php
@@ -1,25 +1,23 @@
 <?php
-	$to = "";
+	$to = "";//"andrkje@stud.ntnu.no";
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$subject = $_POST["subject"];
 	$msg = $_POST["message"];
 	
-	$message = $subject."\n\n".
+	$message =
+		"Subject: ".$subject."\n".
 		"From: ".$name."\n".
 		"Email: ".$email."\n\n".
 		"Message:\n\n".$msg;
 
 	// send email
-	if(!filter_var("someone@example....com", FILTER_VALIDATE_EMAIL) || strlen($to) < 1 || strlen($name) < 1 || strlen($subject) < 1 || strlen($msg) < 1) {		// Not valid input
-		header("Location: ../contact/after.html");
+	if(!filter_var($email, FILTER_VALIDATE_EMAIL) || !filter_var($to, FILTER_VALIDATE_EMAIL) || strlen($name) < 1 || strlen($subject) < 1 || strlen($msg) < 1) {		// Not valid input
+		header("Location: ../contact/error.html");	// Send user to error page
 	}
 	else {
-		
 		mail($to,$subject,$message);				// Send email
 		
 		header("Location: ../contact/after.html");	// Send user to page after mail is sent
 	}
 ?>
-
-
